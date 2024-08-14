55c9f99b8fda0ad0a1b124aedaf576a9ee0057db
Herickito <herickhgs@unipam.edu.br>
2024-08-14 12:29:34 -0300

    ADD: Tela Inicial

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector('#signup');
-
+var btnLogar = document.querySelector('#loginButton');
 
 var body = document.querySelector("body");
 
+btnLogar.addEventListener('click', function() {
+    window.location.href = 'telaInicial.html';  // Redireciona para a tela de login
+});
 
 btnSignin.addEventListener('click', function() {
     body.className = "sign-in-js";
@@ -12,3 +15,5 @@ btnSignin.addEventListener('click', function() {
 btnSignup.addEventListener('click', function() {
     body.className = "sign-up-js";
 });
+
+
