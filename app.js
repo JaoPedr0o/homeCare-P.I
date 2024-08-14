6f28aa2ed47c00c64042581b3572d82924bd93d7
MMprog2005 <gabriel.marra.ribeiro@gmail.com>
2024-08-14 15:14:06 -0300

    Arrumações para o usuário
    -centralizei os ícones das redes sociais, tirei o "esqueceu a senha " da área de registro, modifiquei a unidade de medida usada no CSS (não recomendado usar px para certas coisas), e suavizei um pouco a transição entre o Login e o Cadastro

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ btnLogar.addEventListener('click', function() {
 
 btnSignin.addEventListener('click', function() {
     body.className = "sign-in-js";
+    
 });
 
 btnSignup.addEventListener('click', function() {
