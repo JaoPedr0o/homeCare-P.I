13a5d0f0a50e2d20e64abb6c8500e8874db9937f
MMprog2005 <gabriel.marra.ribeiro@gmail.com>
2024-08-20 15:31:24 -0300

    Arrumação do formulário
    Imagem, texto, fluxo, inputs e selects

--- app.js
+++ app.js
@@ -18,3 +18,55 @@ btnSignup.addEventListener('click', function() {
 });
 
 
+
+// Função para buscar os estados da API do IBGE
+async function buscarEstados() {
+    const url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
+    try {
+        const response = await fetch(url);
+        const estados = await response.json();
+        
+        const selectElement = document.getElementById("estados-select");
+
+        // Ordenar estados por nome
+        estados.sort((a, b) => a.nome.localeCompare(b.nome));
+
+        // Adiciona cada estado como uma opção no select
+        estados.forEach(estado => {
+            const option = document.createElement("option");
+            option.value = estado.sigla;
+            option.textContent = estado.sigla;
+            selectElement.appendChild(option);
+        });
+    } catch (error) {
+        console.error("Erro ao buscar estados:", error);
+    }
+}
+// Chama a função para buscar os estados quando a página carrega
+window.onload = buscarEstados;
+
+
+
+//função para alterar o cadastro de paciente para profissional
+let selectCat = document.getElementById("selectCategoria")
+selectCat.addEventListener('change', () => {
+    valor = selectCat.value
+    if (valor == 2) {
+        function criarLabel(){
+            const item = document.createElement("label")
+            item.innerHTML = `<input type="text" placeholder="Coren" required>`
+            item.classList.add('label-input')
+            item.id = 'labelJS'
+            document.getElementById("formCad").appendChild(item)
+        }
+        criarLabel()
+    }
+    else{
+        function remover(){
+            form = document.getElementById("formCad")
+            label = document.getElementById('labelJS')
+            form.removeChild(label)
+        }
+        remover()
+    }
+})
