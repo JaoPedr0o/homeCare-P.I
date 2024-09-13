bb3a4113448db92acc1432c12f24ba12a4f6dde2
JaoPedr0o <joaopedromp15@outlook.com>
2024-09-13 15:22:25 -0300

    feat: separando css da sideBar / resposividade perfil

--- perfil.php
+++ perfil.php
@@ -5,49 +5,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+          crossorigin="anonymous">
+    
     <script src="https://kit.fontawesome.com/2a79d52758.js" crossorigin="anonymous"></script>
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">  
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" 
+          rel="stylesheet">
+    
     <link rel="stylesheet" href="perfil.css">
     <link rel="stylesheet" href="style.global.css">
+    <link rel="stylesheet" href="sideBar.css.css">
 </head>
 
 <body>
     <div class="container-fluid">
         <div class="row">
-            <?php 
-                require 'sidebar.php'
-            ?>
+            <?php require 'sidebar.php'; ?>
         </div>
     </div>
 
     <main>
         <div class="container">
-            <div class="header">
+            <header class="header">
                 <div class="header-imgs">
-                    <!-- Imagem de perfil -->
-                    <img id="banner-img"src="./assets/user-banner.png" alt="Foto de fundo">
+                    <img id="banner-img" src="./assets/user-banner.png" alt="Foto de fundo">
                     <img id="perfil-img" src="./assets/image.png" alt="Foto de Rasputia Nogueira">
                 </div>
 
                 <div class="header-details">
-                    <!-- Conteúdo da about -->
-                    <div class="about-content">
+                    
+                    <section class="about-content">
                         <h1>Rasputia Nogueira</h1>
                         <h3>Cuidadora</h3>
-                        
                         <p>Rasputia Nogueira, nascida em 1988 em Belo Horizonte, é enfermeira formada pela Universidade Federal de Minas Gerais. Desde 2010, tem se dedicado ao atendimento de pacientes em diversas áreas de saúde, destacando-se por sua empatia e profissionalismo. Em 2015, recebeu o prêmio de "Enfermeira do Ano" pelo Hospital das Clínicas de Belo Horizonte.</p>
-                        
-                        <!--<div class="actions">
-                            <button class="btn">Enviar mensagem</button>
-                            <button class="btn">+ Seguir</button>
-                        </div> -->
-                    </div>
+                    </section>
 
-                    <!-- Seção de informações ao lado direito -->
-                    <div class="info">
+                    <aside class="info">
                         <div class="item">
                             <h3>Estado</h3>
                             <p>Minas Gerais, MG</p>
@@ -68,11 +67,11 @@
                             <h3>Gênero</h3>
                             <p>Feminino</p>
                         </div>
-                    </div>
+                    </aside>
                 </div>
-            </div>
-            <!-- Skills e Resumo Profissional lado a lado -->
-            <div class="skills-and-resume">
+            </header>
+
+            <section class="skills-and-resume">
                 <div class="skills">
                     <h2>Habilidades</h2>
                     <div class="skill">
@@ -100,17 +99,19 @@
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="resume">
                     <h2>Resumo Profissional</h2>
                     <p>Estágio de dois anos no Hospital Nossa Senhora de Fátima, onde desenvolvi habilidades em administração de medicamentos, monitoramento de sinais específicos e assistência em procedimentos médicos. Experiência em diferentes áreas como emergência e UTI, além de competências em comunicação com pacientes.</p>
                 </div>
-            </div>
+            </section>
         </div>
     </main>
 
     <script src="app.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+            crossorigin="anonymous"></script>
 </body>
 
 </html>
