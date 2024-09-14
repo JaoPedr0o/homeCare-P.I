22941d231a940b6f1bacce5f7cb42eed1979aec9
JaoPedr0o <joaopedromp15@outlook.com>
2024-09-14 20:43:15 -0300

    feat: estilizando sidebar desktop/tablet

--- sidebar.php
+++ sidebar.php
@@ -7,7 +7,7 @@
     <title>Tela Inicial</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/2a79d52758.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="sideBar.css">
+    <link rel="stylesheet" href="sidebar.css">
     <link rel="stylesheet" href="/style.global.css">
 </head>
 
@@ -16,39 +16,42 @@
         <div class="sidebar-content">
             <ul class="nav flex-column">
                 <li class="nav-item">
+                    <img class="logo" src="./loginIMG/logo-transparente.png" alt="">
+                    <span class="sidebar-text-logo">VITAL+</span>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link active" href="telaInicial.php">
                         <i class="fa-solid fa-house"></i>
-                        <span class="sidebar-text">Início</span>
+                        <span class="sidebar-text">INÍCIO</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="perfil.php">
                         <i class="fa-solid fa-user"></i>
-                        <span class="sidebar-text">Perfil</span>
+                        <span class="sidebar-text">PERFIL</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="profissionais.php">
                         <i class="fa-solid fa-notes-medical"></i>
-                        <span class="sidebar-text">Profissionais</span>
+                        <span class="sidebar-text">PROFISSIONAIS</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="materiais.php">
                         <i class="fa-solid fa-shop"></i>
-                        <span class="sidebar-text">Materiais</span>
+                        <span class="sidebar-text">MATERIAIS</span>
                     </a>
                 </li>
-                <li class="nav-item" style="position: fixed; bottom: 0;">
+                <li class="nav-item">
                     <a class="nav-link" href="login.php">
-                        <i class="fa-solid fa-door-closed"></i>
-                        <span class="sidebar-text">Sair</span>
+                        <i class="fa-solid fa-gear settings"></i>
                     </a>
                 </li>
             </ul>
-
         </div>
     </div>
+
     <script src="app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
