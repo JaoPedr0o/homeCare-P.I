e010b6bd27602082269216aa4be4a9bbd0980d25
yaszeer <yasminribeiro@unipam.edu.br>
2024-10-20 12:38:00 -0300

    pagina: assinatura

---
+++ assinatura.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assinatura.css"> 
+    <title>Vital+</title>
+</head>
+<body>
+    <h2 class=titulo> Deseja participar da equipe de profissionais capacitados do Vital+?</h2> 
+    <p class=texto> Basta assinar o nosso plano! Com ele, você poderá se conectar com pacientes, os ajudando remotamente ou pessoalmente.
+    <div class="container">
+  <div class="card" style="width: 21rem;">
+    <div class="txtinicial">
+      <p class="txt1">O Vital+ conecta famílias a profissionais da saúde qualificados, oferecendo cuidados práticos e seguros para idosos.</p>
+      <p class="preco">R$11,90 Mensal </p>
+      <a class="btn rounded-1 btn-lg" style="width: 200px" href="https://mpago.la/2FM7JGo" role="button">Assinar</a>
+      <p></p>
+    </div>
+    <div class="txtbaixo">
+      <p class="txt1">
+        Com a assinatura mensal para se integrar como profissional, você poderá auxiliar e encontrar variados pacientes,
+        podendo expandir sua experiência como profissional da saúde e também auxiliar seus pacientes de maneira mais fácil
+        e rápida.
+      </p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
